fac2ec6a6c5b5e5424b29a00848fc633c2804c19
Luiz Damasceno <luizzzz210103@gmail.com>
2025-05-12 21:11:26 -0300

    Finished

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,29 @@ Declare a tuple consisting of the integer, float,
 a Boolean, and the array that you previously declared.
 Print out the tuple in its Debug representation.
 */
-fn main() {}
+#![allow(unused_variables)]
+
+fn main() {
+    let a_number: i32 = 1_337;
+    let another_number: i16 = 1_337;
+    let another_number: i16 = a_number;
+
+    let float_number: f64 = 6.2639017267633;
+    println!("{:.3}", float_number);
+
+    let with_milk: bool = true;
+    let with_sugar: bool = true;
+
+    let is_my_type_of_coffee: bool = with_milk == true && with_sugar == true;
+    let is_acceptable_coffee: bool = with_milk == true || with_sugar == true;
+
+    let array_i8: [i8; 4] = [7, 1, 21, 14];
+    let the_tuple = (5, 3.14, false, array_i8);
+
+    println!("\n\nThe array is:\n{:?}", array_i8);
+    println!("{:#?}", array_i8);
+    dbg!(array_i8);
+    println!("\n\nThe tuple is:\n{:?}", the_tuple);
+    println!("{:#?}", the_tuple);
+    dbg!(the_tuple);
+}
